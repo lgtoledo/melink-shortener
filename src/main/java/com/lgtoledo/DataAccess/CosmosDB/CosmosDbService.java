ae63ab097d87b65e1773044af6db8a2a0eea3eb4
Leandro Toledo <lgtoledo@outlook.com>
2023-10-08 15:42:03 -0300

    se agregan servicios de cosmos DB para stats

--- src/main/java/com/lgtoledo/DataAccess/CosmosDB/CosmosDbService.java
+++ src/main/java/com/lgtoledo/DataAccess/CosmosDB/CosmosDbService.java
@@ -1,34 +1,40 @@
 package com.lgtoledo.DataAccess.CosmosDB;
 
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 import com.azure.cosmos.CosmosClient;
 import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.cosmos.CosmosContainer;
 import com.azure.cosmos.CosmosDatabase;
-import com.azure.cosmos.implementation.Document;
 import com.azure.cosmos.models.CosmosItemRequestOptions;
 import com.azure.cosmos.models.CosmosItemResponse;
 import com.azure.cosmos.models.PartitionKey;
-import com.lgtoledo.Models.LinkModel;
+import com.lgtoledo.Configurations;
+import com.lgtoledo.Models.Link;
+import com.lgtoledo.Models.LinkAccessStat;
 
 public class CosmosDbService {
     private final CosmosClient cosmosClient;
     private final CosmosDatabase cosmosDatabase;
-    private final CosmosContainer cosmosContainer;
+    private final CosmosContainer cosmosLinksContainer;
+    private final CosmosContainer cosmosLinksAccessStatsContainer;
 
-    public CosmosDbService(String endpoint, String key, String databaseName, String containerName) {
+
+    public CosmosDbService(String endpoint, String key, String databaseName) {
         this.cosmosClient = new CosmosClientBuilder()
                 .endpoint(endpoint)
                 .key(key)
                 .buildClient();
         this.cosmosDatabase = cosmosClient.getDatabase(databaseName);
-        this.cosmosContainer = cosmosDatabase.getContainer(containerName);
+        this.cosmosLinksContainer = cosmosDatabase.getContainer(Configurations.COSMOS_DB_LINKS_CONTAINER);
+        this.cosmosLinksAccessStatsContainer = cosmosDatabase.getContainer(Configurations.COSMOS_DB_LINKS_ACCESS_STATS_CONTAINER);
     }
 
-    public Optional<LinkModel> getLinkByShortId(String shortLinkId) {
+    // Links
+    public Optional<Link> getLinkByShortId(String shortLinkId) {
         try {
-            CosmosItemResponse<LinkModel> response = cosmosContainer.readItem(shortLinkId, new PartitionKey(shortLinkId), LinkModel.class);
+            CosmosItemResponse<Link> response = cosmosLinksContainer.readItem(shortLinkId, new PartitionKey(shortLinkId), Link.class);
             
             return Optional.ofNullable(response.getItem());
         } catch (Exception e) {
@@ -37,12 +43,10 @@ public class CosmosDbService {
         }
     }
 
-    public LinkModel saveLink(LinkModel link) {
+    public Link saveLink(Link link) {
         try {
-             System.out.println("Guardando link: " + link.toJson());
-
-            Document document = new Document(link.toJson());
-            CosmosItemResponse<Document> itemResponse = cosmosContainer.createItem(document);
+            // Document document = new Document(link.toJson());
+            CosmosItemResponse<Link> itemResponse = cosmosLinksContainer.createItem(link);
 
             return itemResponse.getStatusCode() == 201 ? link : null;
 
@@ -54,31 +58,97 @@ public class CosmosDbService {
     public void deleteLink(String shortLinkId) {
         try {
             CosmosItemRequestOptions options = new CosmosItemRequestOptions();
-            cosmosContainer.deleteItem(shortLinkId, new PartitionKey(shortLinkId), options);
+            cosmosLinksContainer.deleteItem(shortLinkId, new PartitionKey(shortLinkId), options);
   
         } catch (Exception e) {
             return;
         }        
     }
 
-    public void deleteAllLinks() {
-        try {
-            boolean containerExists = cosmosDatabase.readAllContainers().stream()
-            .anyMatch(containerProperties -> containerProperties.getId().equals("links"));
-    
-            if (containerExists) {
-                cosmosDatabase.getContainer("links").delete();
-            }
-            cosmosDatabase.createContainerIfNotExists("links", "/id");
+    public void deleteAllLinksAsync() {
+        CompletableFuture.runAsync(() -> {
+            try {
+                boolean containerExists = cosmosDatabase.readAllContainers().stream()
+                .anyMatch(containerProperties -> containerProperties.getId().equals("links"));
+        
+                if (containerExists) {
+                    cosmosLinksContainer.delete();
+                }
+                cosmosDatabase.createContainerIfNotExists("links", "/id");
 
+            } catch (Exception e) {
+                return;
+            }
+        });
+    }
+
+    public boolean existsLinkById(String shortLinkId) {
+        try {
+            cosmosLinksContainer.readItem(shortLinkId, new PartitionKey(shortLinkId), Link.class);
+            
+            return true;  // Si no hay errores, existe
         } catch (Exception e) {
-            return;
+                
+            return false;
         }
     }
 
-    public boolean existsById(String shortLinkId) {
+    // Link Access Stats
+    public Optional<LinkAccessStat> getLinkAccessStatById(String shortLinkId) {
         try {
-            cosmosContainer.readItem(shortLinkId, new PartitionKey(shortLinkId), LinkModel.class);
+            CosmosItemResponse<LinkAccessStat> response = cosmosLinksAccessStatsContainer.readItem(shortLinkId, new PartitionKey(shortLinkId), LinkAccessStat.class);
+            
+            return Optional.ofNullable(response.getItem());
+        } catch (Exception e) {
+            System.out.println("Error al obtener estadística: " + e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+    public LinkAccessStat saveLinkAccessStat(LinkAccessStat linkAccessStat) {
+        try {
+            // Document document = new Document(linkAccessStat);
+            // CosmosItemResponse<Document> itemResponse = cosmosLinksAccessStatsContainer.upsertItem(document);
+            CosmosItemResponse<LinkAccessStat> itemResponse = cosmosLinksAccessStatsContainer.upsertItem(linkAccessStat);
+
+            return itemResponse.getStatusCode() == 200 || itemResponse.getStatusCode() == 201 ? itemResponse.getItem() : null;
+
+        } catch (Exception e) {
+            System.out.println("Error al obtener estadística: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public void deleteLinkAccessStat(String shortLinkId) {
+        try {
+            CosmosItemRequestOptions options = new CosmosItemRequestOptions();
+            cosmosLinksAccessStatsContainer.deleteItem(shortLinkId, new PartitionKey(shortLinkId), options);
+  
+        } catch (Exception e) {
+            return;
+        }        
+    }
+
+    public void deleteAllLinkAccessStatsAsync() {
+        CompletableFuture.runAsync(() -> {
+            try {
+                boolean containerExists = cosmosDatabase.readAllContainers().stream()
+                .anyMatch(containerProperties -> containerProperties.getId().equals("linksAccessStats"));
+        
+                if (containerExists) {
+                    cosmosLinksAccessStatsContainer.delete();
+                }
+                cosmosDatabase.createContainerIfNotExists("linksAccessStats", "/id");
+
+            } catch (Exception e) {
+                return;
+            }
+        });
+    }
+
+    public boolean existsLinkAccessStatById(String shortLinkId) {
+        try {
+            cosmosLinksAccessStatsContainer.readItem(shortLinkId, new PartitionKey(shortLinkId), LinkAccessStat.class);
             
             return true;  // Si no hay errores, existe
         } catch (Exception e) {
